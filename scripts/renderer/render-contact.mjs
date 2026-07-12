1ece738c542eaab6407fb913da8b05621dfb6ace
JosephJMWalker-MBA <joseph@decisionpointinitiative.com>
2026-07-12 13:26:41 -0400

    Complete contact composer

--- scripts/renderer/render-contact.mjs
+++ scripts/renderer/render-contact.mjs
@@ -1,11 +1,8 @@
 import { renderLocation } from './render-location.mjs';
 import { renderPhone } from './render-phone.mjs';
 import { renderEmail } from './render-email.mjs';
-import { renderLink } from './render-link.mjs';
+import { renderNamedLinks } from './render-named-links.mjs';
 
 export const renderContact = (contact = {}) => {
-  const items = [
-    renderLocation(contact.location),
-    renderPhone(contact.phone),
-    renderEmail(contact.email),
-    renderLink('LinkedIn',
+  const items = [renderLocation(contact.location), renderPhone(contact.phone), renderEmail(contact.email), ...renderNamedLinks(contact)].filter(Boolean);
+  return items.length ?
